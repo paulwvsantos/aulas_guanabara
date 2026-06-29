f0beb8697bcdad5584b0dcdb85bb04de8f1a3217
Paul <paul.wvsantos@senacsp.edu.br>
2026-06-28 23:28:38 -0300

    Terceiro commit: Aulas 9 a 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 68.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+     console.log('Você passou a velocidade permitida. MULTADO')
+}
+console.log('Dirija sempre usando cinto de segurança')
