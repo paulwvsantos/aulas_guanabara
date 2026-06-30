aca5f927cb88f68ba63738e50a127087e5d03da6
PAUL WILSON VENUDIO DOS SANTOS <paul.wvsantos@senacsp.edu.br>
2026-06-29 22:02:13 -0300

    Quarto commit: Aula 12 e exercicios 1 e 2

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // Bom Dia!
+        img.src = 'manha.png'
+        document.body.style.background = '#89A3C0'
+    } else if (hora >= 12 && hora < 18) {
+        // Boa Tarde!
+        img.src = 'tarde.png'
+        document.body.style.background = '#FFC600'
+    } else {
+        // Boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#8A6D8C'
+    }
+}
